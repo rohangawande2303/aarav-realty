3d36913c21aa0b5ca40d626f8d38919943d29265
Rohan <rohangawande2303@gmail.com>
2024-08-29 16:14:11 +0530

    whatsapp api and mobile banner img update

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -4,24 +4,52 @@ import Image from "next/image";
 import sample1 from "../../images/banner-1.jpg";
 import sample2 from "../../images/banner-2.jpg";
 import sample3 from "../../images/banner-3.jpg";
+import mobile1 from "../../images/mob-banner-1.jpg";
+import mobile2 from "../../images/mob-banner-2.jpg";
+import mobile3 from "../../images/mob-banner-3.jpg";
 
-const images = [sample1, sample2, sample3];
+const desktopImages = [sample1, sample2, sample3];
+const mobileImages = [mobile1, mobile2, mobile3];
 
 const Banner = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust breakpoint for mobile
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setCurrentIndex(
+        (prevIndex) =>
+          (prevIndex + 1) %
+          (isMobile ? mobileImages.length : desktopImages.length)
+      );
     }, 4000); // Change image every 4 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isMobile]);
+
+  const imagesToShow = isMobile ? mobileImages : desktopImages;
 
   return (
-    <div className="relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[80vh] mt-20">
+    <div
+      className={`relative w-full ${
+        isMobile
+          ? "h-[100vh]"
+          : "h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[80vh]"
+      } mt-20`}
+    >
       <div className="absolute inset-0 w-full h-full">
-        {images.map((image, index) => (
+        {imagesToShow.map((image, index) => (
           <div
             key={index}
             className={`absolute inset-0 ${
--- src/app/components/WhatsappButton.jsx
+++ src/app/components/WhatsappButton.jsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsappButton() {
   return (
     <a
-      href="https://wa.me/yourwhatsappnumber"
+      href="https://api.whatsapp.com/send?phone=7506929394&text=I%20came%20across%20your%20website%20,I%20would%20like%20to%20have%20more%20information%20about%20your%20business"
       className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
       target="_blank"
       rel="noopener noreferrer"
